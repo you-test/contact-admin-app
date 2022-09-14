de5a39ab0e4a12b3ba3f8e06cfe2fba61d1ab2c8
yusuke <sato11.jp@gmail.com>
2022-09-15 02:18:09 +0900

    create database tables contact_data, data_logs finished styles of contact/register.php

---
+++ sql/contact_admin.sql
@@ -0,0 +1,21 @@
+-- create data table
+CREATE TABLE contact_data (
+  contact_id SERIAL PRIMARY KEY,
+  received_date DATETIME NOT NULL,
+  status VARCHAR(16) NOT NULL,
+  user_id INT NOT NULL,
+  mail VARCHAR(256),
+  title VARCHAR(1000) NOT NULL,
+  content VARCHAR(2000),
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- create action log
+CREATE TABLE action_logs (
+  id SERIAL PRIMARY KEY,
+  content VARCHAR(2000),
+  contact_id INT NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
