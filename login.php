cf0ab06a8462e54191500996364efefa53270530
yusuke <sato11.jp@gmail.com>
2022-11-27 03:00:57 +0900

    create login.php create views/login/tmp_login.php

---
+++ login.php
@@ -0,0 +1,3 @@
+<?php
+
+include __DIR__ . '/views/login/tmp_login.php';
---
+++ views/login/tmp_login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Admin</title>
+    <link rel="stylesheet" href="../../html/styles.css">
+</head>
+<body>
+    <header>
+        <h1>お問い合わせ管理</h1>
+    </header>
+    <div class="container">
+        <h2>ログイン</h2>
+        <form action="../../actions/login/action_login.php" method="post">
+            <label for="mail">メールアドレス</label>
+            <input type="mail" name="mail" placeholder="example@xxx.com">
+            <label for="pass">パスワード</label>
+            <input type="password" name="pass" id="pass">
+            <button type="submit">ログイン</button>
+        </form>
+    </div>
+</body>
+</html>
